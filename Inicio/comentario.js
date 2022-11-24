fab47fa138a9f4be067dd756e905bbb448c72ae2
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-11-24 13:32:15 -0300

    Update comentario.js

--- Inicio/comentario.js
+++ Inicio/comentario.js
@@ -1,1 +1,11 @@
-console.log('Comentarios no codigo!');
+// Console.log é usado para exibir algo na tela do computador!
+console.log('Comentarios no javaScript!');
+console.log('Comentarios no Codigo');
+console.log('Para comentar uma linha de codigo utilize //  ou /**/'); /* Mais um explo de comentario */
+
+/* 
+* Esse é um
+* comentario
+* de mútiplas
+* linhas!
+*/
