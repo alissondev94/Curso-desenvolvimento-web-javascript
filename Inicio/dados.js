9d58beabaec3144f59bc3b9b8d610f5aa2093a5b
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-11-25 17:19:45 -0300

    Update dados.js

--- Inicio/dados.js
+++ Inicio/dados.js
@@ -1,11 +1,14 @@
-var nomeProduto = "caneta";
-var quantidade = 10;
-var preco = 6.4;
-var imposto = 1.5;
-var precoFinal = preco + imposto;
+let nomeProduto = "caneta";
+let quantidade = 10;
+let preco = 6.4;
+let imposto = 1.5;
+let precoFinal = preco + imposto;
 
 console.log(nomeProduto);
 console.log(quantidade);
 console.log(preco);
 console.log(imposto);
 console.log(precoFinal);
+
+nomeProduto = "Caneta BIC"
+console.log(nomeProduto);
