434e4109049f38d30764c71a8f9f5133d10ff39f
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-11-29 15:16:18 -0300

    Update dados_2.js

--- Inicio/dados_2.js
+++ Inicio/dados_2.js
@@ -1,7 +1,14 @@
 let preco = 19.90;
 let desconto = 0.4;
-
-console.log(`o preço com desconto é igual R$${preco * (1 - desconto)}`);
-
 let precoComDesconto = preco * (1 - desconto);
-console.log(`O preço com desconto é igual R$${precoComDesconto}`)
+console.log(`preço com desconto R$${precoComDesconto}`);
+
+let produto = 'caderno';
+let categoria = 'papelaria';
+console.log(
+    "Produto: " + produto
+    + ", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto
+    + ", Total a Pagar: " + precoComDesconto
+);
