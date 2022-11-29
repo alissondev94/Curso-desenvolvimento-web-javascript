b5ea7483d79a6b46c572883d40087f081dc84976
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-11-29 14:31:54 -0300

    atualização

--- Inicio/comentario.js
+++ Inicio/comentario.js
@@ -3,9 +3,10 @@ console.log('Comentarios no javaScript!');
 console.log('Comentarios no Codigo');
 console.log('Para comentar uma linha de codigo utilize //  ou /**/'); /* Mais um explo de comentario */
 
-/* 
+/*
 * Esse é um
 * comentario
 * de mútiplas
 * linhas!
 */
+
