999500826b4efecfbda96de7d09f655e4cc4defa
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-11-29 16:11:00 -0300

    Create dados_3.js

---
+++ Inicio/dados_3.js
@@ -0,0 +1,12 @@
+let idade = 28;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 3970.56;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
+
+console.log(typeof "Teste");
+console.log(typeof 'Teste');
