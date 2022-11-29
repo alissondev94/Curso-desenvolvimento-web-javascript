554f58b4d3ef0deb11112fc10670ad4893f72f12
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-11-29 14:57:47 -0300

    Update dados_2.js

---
+++ Inicio/dados_2.js
@@ -0,0 +1,7 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(`o preço com desconto é igual R$${preco * (1 - desconto)}`);
+
+let precoComDesconto = preco * (1 - desconto);
+console.log(`O preço com desconto é igual R$${precoComDesconto}`)
