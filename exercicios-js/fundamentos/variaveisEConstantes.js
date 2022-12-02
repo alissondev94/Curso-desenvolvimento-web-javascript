538d71b86cfec1cd3dd47e5d618ba1b61f0a0fa6
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-12-02 12:53:07 -0300

    Create variaveisEConstantes.js

---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,10 @@
+var a = 3
+let b = 4 // Optar usar let
+
+var a = 30
+b = 40
+
+console.log('a, b')
+
+// trabalhar com let e mais aconselhado do que com var
+// a variavel var pode ser redeclarada varias vezes enquanto let não
