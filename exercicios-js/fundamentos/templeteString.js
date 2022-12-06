61c6a792f65950749fb3785b7750c63890c08c67
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-12-06 10:56:32 -0300

    Create templeteString.js

---
+++ exercicios-js/fundamentos/templeteString.js
@@ -0,0 +1,18 @@
+const nome = 'Itamara'
+const concatenacao = 'Olá ' + nome + '!'
+const template = ` Olá ${nome} !`
+const template1 = `
+    Olá
+    ${nome}!`
+
+    console.log(concatenacao)
+    console.log(template)
+    console.log(template1)
+
+    //expressoes...
+
+    console.log(`25 * 5 = ${25 * 5}`)
+
+    const up = texto => texto.toUpperCase()
+
+    console.log(`Ei... ${up('Cuidado')}!`)
