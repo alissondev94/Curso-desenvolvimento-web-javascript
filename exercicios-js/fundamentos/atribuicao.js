fe842afd1e36c3e3ee86be4ba050241507bc9f3a
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-12-26 16:14:58 -0300

    Update atribuicao.js

--- exercicios-js/fundamentos/atribuicao.js
+++ exercicios-js/fundamentos/atribuicao.js
@@ -4,14 +4,14 @@ let b = 3
 b += a // b = b + a
 console.log(b)
 
-b -= 4 b // b - 4
+b -= 4 // b = b - 4
 console.log(b)
 
-b *= 2 // b * 2
+b *= 2 // b = b * 2
 console.log(b)
 
-b /=2 // b / 2
+b /=2 // b = b / 2
 console.log(b)
 
-b %= 2 // b = b % 2 se o resultado for par numero vai ser 0 se o resultado for impar numero 1
+b %= 2 // b = b % 2 result for par imprime 0 reslt for impar imprime 1
 console.log(b)
