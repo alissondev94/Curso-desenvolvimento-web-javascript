98ac2703159550c629ca7929b02be7f765732f9b
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-12-26 16:11:01 -0300

    Create atribuicao.js

---
+++ exercicios-js/fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a // b = b + a
+console.log(b)
+
+b -= 4 b // b - 4
+console.log(b)
+
+b *= 2 // b * 2
+console.log(b)
+
+b /=2 // b / 2
+console.log(b)
+
+b %= 2 // b = b % 2 se o resultado for par numero vai ser 0 se o resultado for impar numero 1
+console.log(b)
