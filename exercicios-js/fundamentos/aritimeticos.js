22d5439a3456c9578f3b9584ab406d2038c35964
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-12-27 11:21:31 -0300

    Create aritimeticos.js

---
+++ exercicios-js/fundamentos/aritimeticos.js
@@ -0,0 +1,13 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2 // muito usado para saber se o numero e par ou impar
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
+
+// op1 + op2 = operador binário / sintase enfixa
+// -Op1 = operador unário / prefix
+// op2+ operador unário // posfix
