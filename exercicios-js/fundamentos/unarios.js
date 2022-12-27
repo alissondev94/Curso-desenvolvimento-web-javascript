59adaf79dc4a46227a49e52848d86238a313e29e
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-12-27 15:17:16 -0300

    Create unarios.js

---
+++ exercicios-js/fundamentos/unarios.js
@@ -0,0 +1,13 @@
+let num1 = 1
+let num2 = 2
+
+// Exemplos de operadores Unários
+
+num1++ // Pos fixado
+console.log(num1)
+
+--num1 // prefixada
+console.log(num1)
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
