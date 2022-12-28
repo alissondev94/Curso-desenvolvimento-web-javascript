12d589dc7b5dcb1c6ff277c5546f3eeb9e873cef
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-12-28 11:23:30 -0300

    Create ifElse.js

---
+++ exercicios-js/controle/ifElse.js
@@ -0,0 +1,12 @@
+const imprimirResultado = function(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado!')
+    } else{
+        console.log('Reprovado!')
+    } if(nota >= 9) {
+        console.log('congratulation!')
+    }
+}
+
+imprimirResultado(6.8)
+imprimirResultado(9.2)
