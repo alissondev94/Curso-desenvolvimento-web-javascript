8d01d9c1531aeecd9470133e783c2808a2171efb
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-12-29 10:09:05 -0300

    Update breakContinue.js

--- exercicios-js/controle/breakContinue.js
+++ exercicios-js/controle/breakContinue.js
@@ -1,6 +1,22 @@
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for( let n in nums){
-    if(n == 5) break
-    console.log(`n = ${nums[n]}`)
+    if(n == 5) {
+        break
+    }
+    console.log(`${n} = ${nums[n]}`)
+}
+
+for(let n2 in nums) {
+    if( n2 == 5) {
+        continue
+    }
+    console.log(`.${n2} = ${nums[n2]}`)
+}
+
+externo: for (let n3 in nums){
+    for (let n4 in nums){
+        if (n3 == 2 && n4 == 3) break externo
+        console.log(`par = ${n3},${n4}`)
+    }
 }
