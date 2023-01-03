77cd875aebd9f7bd6b685dc9762f4c7ba6daaa59
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2023-01-03 17:24:39 -0300

    Create arrowFunction3.js

---
+++ exercicios-js/funcao/arrowFunction3.js
@@ -0,0 +1,18 @@
+let comparaComThis = function (param) {
+    console.log(this === param)
+}
+
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+
+comparaComThisArrow(module.exports)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+
+comparaComThisArrow(obj)
