7c331208207de771aa2ee366b74d319157e4114a
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2023-01-05 16:11:26 -0300

    Create tiposDeclaracoes.js

---
+++ exercicios-js/funcao/tiposDeclaracoes.js
@@ -0,0 +1,14 @@
+// Function declaration
+function soma(x, y) {
+    return x + y
+}
+
+// Function expression
+const sub = function (x, y)c{
+    return x - y
+}
+
+// Named Function expression
+const mult = function mult(x, y){
+    return x * y
+}
