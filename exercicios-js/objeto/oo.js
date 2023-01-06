b8466795eb2cb748bf356e8dcf83f994fa618741
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2023-01-06 17:41:20 -0300

    Create oo.js

---
+++ exercicios-js/objeto/oo.js
@@ -0,0 +1,20 @@
+// CÓDIGO NÂO EXECUTÁVEL!!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento() // foco passou a ser o objeto
+
+// Princípios importantes:
+// 1º abstração
+// 2º encapsulamento
