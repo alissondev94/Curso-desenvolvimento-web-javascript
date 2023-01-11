b812860c004c4e67316cf9cdc53f9dcac8d8e44a
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2023-01-11 09:47:44 -0300

    Update funcoesImportantes.js

--- exercicios-js/objeto/funcoesImportantes.js
+++ exercicios-js/objeto/funcoesImportantes.js
@@ -17,4 +17,19 @@ Object.entries(pessoa).forEach(([chave, valor]) => {
     console.log(`${chave}: ${valor}`)
 })
 
-Object.defineProperty()
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true,
+    writable: false,
+    value: '02/10/1994'
+})
+
+pessoa.dataNascimento = '02/10/1996'
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
+
+// Object.assing (ECMAScript 2015 - ES6)
+const dest = { a: 1 }
+const o1 = { b: 1 }
+const o2 = { c: 3, a: 4 }
+const obj = Object.assign(dest, o1, o2,)
+console.log(dest)
